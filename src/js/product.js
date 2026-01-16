7d0bdefde57d3442dce27df357751499f4c01a3c
Dragnum <dragnumexp@gmail.com>
2026-01-16 10:21:28 -0300

    team activity finished

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,11 @@
+import { getParam } from "./utils.mjs";
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
+
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || [];
